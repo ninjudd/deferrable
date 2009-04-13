671192ea1b84d9eb72b12e50e4b07501085041b0
Justin Balthrop <justin@geni.com>
2009-04-13 11:41:43 -0700

    now_and_later + clear_deferred

--- lib/deferrable.rb
+++ lib/deferrable.rb
@@ -7,8 +7,17 @@ module Deferrable
     end
   end
 
+  def now_and_later(&block)
+    deferred(false, &block)
+    deferred(true, &block)
+  end
+
   def complete_deferred
     deferred_blocks.each {|block| block.call}
+    clear_deferred
+  end
+
+  def clear_deferred
     deferred_blocks.clear
   end
 
